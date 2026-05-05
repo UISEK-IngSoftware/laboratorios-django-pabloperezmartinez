f19bcab33359fe459384c96cf180020ab3b35b9d
Pablo <pablo.perez@uisek.edu.ec>
2026-05-05 10:47:25 -0500

    Laboratorio 4 finalizado!!!

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    type = models.CharField(max_length=50, null=False)
+    height = models.FloatField()
+    weight = models.FloatField()
+    
+    def __str__(self):
+        return self.name
