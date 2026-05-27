0fd4f9d94c3700fbf174e05340a5244b9f28b76b
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2026-05-26 19:08:25 -0500

    Añade autenticación

--- lab8/settings.py
+++ lab8/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Path to redirect after login
+LOGIN_REDIRECT_URL = 'pokedex:index'
+LOGOUT_REDIRECT_URL = 'pokedex:index'
+LOGIN_URL = 'pokedex:login'
--- pokedex/views.py
+++ pokedex/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render
 from django.template import loader
 from .models import Pokemon
 from .forms import PokemonForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     pokemons = Pokemon.objects.order_by('name')
@@ -12,6 +14,7 @@ def pokemon(request, id: int):
     pokemon = Pokemon.objects.get(pk=id)
     return render(request, 'display_pokemon.html', {'pokemon': pokemon})
 
+@login_required
 def add_pokemon(request):
     if request.method == 'POST':
         form = PokemonForm(request.POST, request.FILES)
@@ -24,6 +27,7 @@ def add_pokemon(request):
             
     return render (request, 'pokemon_form.html', {'form': form})
 
+@login_required
 def edit_pokemon(request, id: int):
     pokemon = Pokemon.objects.get(pk=id)
     if request.method == 'POST':
@@ -36,7 +40,11 @@ def edit_pokemon(request, id: int):
         form = PokemonForm(instance=pokemon)
     return render (request, 'pokemon_form.html', {'form': form})
 
+@login_required
 def delete_pokemon(request, id: int):
     pokemon = Pokemon.objects.get(pk=id)
     pokemon.delete()
     return redirect('pokedex:index')
+
+class CustomLoginView(LoginView):
+    template_name = 'login_form.html'
