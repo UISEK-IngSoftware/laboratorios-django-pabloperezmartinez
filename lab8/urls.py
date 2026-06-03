8078b808f6291f9089a2742f8fad0009a2e74d5b
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2026-06-02 19:10:03 -0500

    API REST implementada

--- lab8/urls.py
+++ lab8/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pokedex.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('api/', include('api.urls')),
 ] 
 
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
