4f5aa6cf88ba1e77363005e5eb0e507820792c80
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2026-06-09 19:33:45 -0500

    Django Oauth toolkit

--- lab8/urls.py
+++ lab8/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from oauth2_provider import urls as oauth2_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pokedex.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
+    path('o/', include(oauth2_urls)),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
